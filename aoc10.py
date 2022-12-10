0fb465ea10f46d87f849f841eb217ca2b2fdab00
moerbeke <aceballos@gmail.com>
2022-12-10 07:53:54 +0100

    Day 10 proper

--- aoc10.py
+++ aoc10.py
@@ -44,9 +44,7 @@ class Cpu:
         self.program = program
         t = 1
         while t <= cycles[-1]:
-            print(t,self.x)
             if t in cycles:
-                print("--->",t,self.x,t*self.x)
                 self.signal_strength += t * self.x
             if self.instruction_in_cpu is None:
                 self.load_next_instruction()
@@ -54,13 +52,37 @@ class Cpu:
             self.run_instruction()
             t += 1
 
+    def run_instruction(self):
+        assert(self.instruction_in_cpu is not None)
+        complete = False
+        if self.instruction_in_cpu[0] == 'noop':
+            if self.instruction_time_in_cpu == 1:
+                complete = True
+        elif self.instruction_in_cpu[0] == 'addx':
+            if self.instruction_time_in_cpu == 2:
+                self.x += int(self.instruction_in_cpu[1])
+                complete = True
+        else:
+            assert(False)
+        if complete:
+            self.instruction_in_cpu = None
+
+    def load_next_instruction(self):
+        assert(len(self.program) >= 1)
+        instruction_line = self.program[0].strip()
+        del self.program[0]
+        self.instruction_in_cpu = instruction_line.split(' ')
+        self.instruction_time_in_cpu = 0
+
+    def get_signal_strength(self):
+        return self.signal_strength
+
     def run_crt(self, program):
         self.program = program
         self.crt_x = 40
         self.crt_y = 6
         self.crt = ''
         for t in range(1, self.crt_x * self.crt_y+1):
-            print(t,self.x)
             if self.instruction_in_cpu is None:
                 self.load_next_instruction()
             self.instruction_time_in_cpu += 1
@@ -82,36 +104,7 @@ class Cpu:
                 printable += self.crt[i]
                 i += 1
             printable += "\n"
-        print(printable)
         return printable
-
-    def run_instruction(self):
-        assert(self.instruction_in_cpu is not None)
-        complete = False
-        print("  run instruction", self.instruction_in_cpu)
-        if self.instruction_in_cpu[0] == 'noop':
-            if self.instruction_time_in_cpu == 1:
-                complete = True
-        elif self.instruction_in_cpu[0] == 'addx':
-            if self.instruction_time_in_cpu == 2:
-                self.x += int(self.instruction_in_cpu[1])
-                complete = True
-        else:
-            assert(False)
-        if complete:
-            self.instruction_in_cpu = None
-
-    def load_next_instruction(self):
-        assert(len(self.program) >= 1)
-        instruction_line = self.program[0].strip()
-        del self.program[0]
-        #print(instruction_line)
-        self.instruction_in_cpu = instruction_line.split(' ')
-        print("  load instruction", self.instruction_in_cpu)
-        self.instruction_time_in_cpu = 0
-
-    def get_signal_strength(self):
-        return self.signal_strength
 
 ########################################################################
 # Test class
@@ -274,155 +267,7 @@ noop
 """, 13140),
                 ]
         self.tc_2 = [
-                (
-"""
-addx 15
-addx -11
-addx 6
-addx -3
-addx 5
-addx -1
-addx -8
-addx 13
-addx 4
-noop
-addx -1
-addx 5
-addx -1
-addx 5
-addx -1
-addx 5
-addx -1
-addx 5
-addx -1
-addx -35
-addx 1
-addx 24
-addx -19
-addx 1
-addx 16
-addx -11
-noop
-noop
-addx 21
-addx -15
-noop
-noop
-addx -3
-addx 9
-addx 1
-addx -3
-addx 8
-addx 1
-addx 5
-noop
-noop
-noop
-noop
-noop
-addx -36
-noop
-addx 1
-addx 7
-noop
-noop
-noop
-addx 2
-addx 6
-noop
-noop
-noop
-noop
-noop
-addx 1
-noop
-noop
-addx 7
-addx 1
-noop
-addx -13
-addx 13
-addx 7
-noop
-addx 1
-addx -33
-noop
-noop
-noop
-addx 2
-noop
-noop
-noop
-addx 8
-noop
-addx -1
-addx 2
-addx 1
-noop
-addx 17
-addx -9
-addx 1
-addx 1
-addx -3
-addx 11
-noop
-noop
-addx 1
-noop
-addx 1
-noop
-noop
-addx -13
-addx -19
-addx 1
-addx 3
-addx 26
-addx -30
-addx 12
-addx -1
-addx 3
-addx 1
-noop
-noop
-noop
-addx -9
-addx 18
-addx 1
-addx 2
-noop
-noop
-addx 9
-noop
-noop
-noop
-addx -1
-addx 2
-addx -37
-addx 1
-addx 3
-noop
-addx 15
-addx -21
-addx 22
-addx -6
-addx 1
-noop
-addx 2
-addx 1
-noop
-addx -10
-noop
-noop
-addx 20
-addx 1
-addx 2
-addx 2
-addx -6
-addx -11
-noop
-noop
-noop
-""",
+                (self.tc_1[0][0],
 """
 ##..##..##..##..##..##..##..##..##..##..
 ###...###...###...###...###...###...###.
